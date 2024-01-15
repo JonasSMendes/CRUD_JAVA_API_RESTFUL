613fa4928b43118fdf45f5768dc3087abfc0db18
jonas mendes <jonas.jsm2903@gmail.com>
2024-01-15 09:13:24 -0300

    process : application

---
+++ src/main/java/com/example/SpringbootApiRest/SpringbootApiRestApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringbootApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiRestApplication.class, args);
+	}
+
+    
+}
